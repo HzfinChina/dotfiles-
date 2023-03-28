925745eda73deebd3f44030340a2b648885d5919
hzf <888iagiag888@gmail.com>
2023-03-28 20:37:34 +0800

    rimeconfig

--- make.sh
+++ make.sh
@@ -5,7 +5,11 @@ ln -fs ~/dotfiles/kitty ~/.config/kitty
 ln -fs ~/dotfiles/neofetch ~/.config/neofetch
 ln -fs ~/dotfiles/npmrc ~/.npmrc
 ln -fs ~/dotfiles/ranger ~/.config/ranger
+# rime
 ln -fs ~/dotfiles/rime ~/.local/share/fcitx5/rime
+cd ~/.local/share/fcitx5/rime
+rime_deployer --build
+
 ln -fs ~/dotfiles/sway ~/.config/sway
 ln -fs ~/dotfiles/waybar ~/.config/waybar
 ln -fs ~/dotfiles/wofi ~/.config/wofi
