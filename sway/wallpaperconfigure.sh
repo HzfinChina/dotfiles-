b58dedbefbd07234168a5cd78b27072ecd52b30b
hzf <888iagiag888@gmail.com>
2023-04-12 22:18:08 +0800

    refresh normally

--- sway/wallpaperconfigure.sh
+++ sway/wallpaperconfigure.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 wpdir="/home/hzf/Pictures/Wallpapers/"
 wallpapers=$(ls $wpdir | sort -R)
+start=true
 while true
 do 
     for file in $wallpapers
     do
+        pkill swaybg
         echo $wpdir/$file > /tmp/WallpaperNow.txt
-        swaybg -m fill -i ${wpdir}${file} 
+        swaybg -m fill -i ${wpdir}${file} &
         sleep 1000
     done
 done
