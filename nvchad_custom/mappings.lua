319dd70c9ed750eba096d8744e66523553f1555f
hzf <888iagiag888@gmail.com>
2023-05-02 16:59:03 +0800

    delete mapping

--- nvchad_custom/mappings.lua
+++ nvchad_custom/mappings.lua
@@ -3,7 +3,6 @@ local M = {}
 
 M.general = {
   n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<F1>"] = {":Alpha<CR>", "open dashboard", opts = {nowait = true}},
   },
 }
